2c7d6e6a85bba38ab05ae930a196bf3e232f2352
Rémy Abi-Khalil <remyabi@gmail.com>
2014-03-21 13:10:17 +0100

    Base of NavitiaProviler

---
+++ DataCollector/NavitiaDataCollector.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace CanalTP\NavitiaProfilerBundle\DataCollector;
+
+use Symfony\Component\HttpKernel\DataCollector\DataCollector;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+class NavitiaDataCollector extends DataCollector
+{
+    private $navitiaAdapter = null;
+
+    public function __construct(NavitiaAdapter $navitiaAdapter)
+    {
+        $this->navitiaAdapter = $navitiaAdapter;
+        $this->data = array(
+            'request_number' => 0,
+            'requests' => array()
+        );
+    }
+
+    public function getRequestNumber()
+    {
+        return ($this->data['request_number']);
+    }
+
+    public function getRequests()
+    {
+        return ($this->data['requests']);
+    }
+
+    public function collect(Request $request, Response $response, \Exception $exception = null)
+    {
+        $this->data['request_number'] = $this->navitiaAdapter->getRequestNumber();
+        $this->data['requests'] = $this->navitiaAdapter->getRequests();
+    }
+
+    public function getName()
+    {
+        return 'navitia';
+    }
+}
---
+++ NavitiaProfilerBundle.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace CanalTP\NavitiaProfilerBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class NavitiaProfilerBundle extends Bundle
+{
+}
